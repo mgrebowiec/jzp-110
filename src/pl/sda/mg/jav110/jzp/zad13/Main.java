6ccdda0403a3f027afcceb1a69aa9ea45322a583
mgrebowiec <michal.grebowiec@gmail.com>
2022-01-16 16:01:22 +0100

    zadanie 13

---
+++ src/pl/sda/mg/jav110/jzp/zad13/Main.java
@@ -0,0 +1,62 @@
+package pl.sda.mg.jav110.jzp.zad13;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> randomList = generateRandomList();
+        System.out.println("Rozmiar calej listy: " + randomList.size());
+        List<Integer> result = randomList.stream().distinct().collect(Collectors.toList());
+        System.out.println("#1: " + result);
+        System.out.println("Rozmiar listy unikalnej: " + result.size());
+
+
+        List<Integer> result2 = getUniqueValues(randomList);
+        System.out.println("#2: " + result2);
+        System.out.println("Rozmiar naszej listy unikalnej: " + result2.size());
+
+        List<Integer> repeatedValues = getRepeatedValues(randomList);
+        System.out.println("Rozmiar listy z powtarzajacymi sie elementami: " + repeatedValues.size());
+
+        List<Integer> result3 = get25(randomList);
+        System.out.println("25 najczesciej powtarzajacyh sie liczb: " + result3);
+    }
+
+    private static List<Integer> get25(List<Integer> values) {
+        return getMap(values).entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(25)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> generateRandomList() {
+        Random r = new Random();
+        List<Integer> randomList = new ArrayList<>();
+        for (int i = 0; i < 100_000; i++) {
+            randomList.add(r.nextInt(100_000));
+        }
+        return randomList;
+    }
+
+    private static List<Integer> getRepeatedValues(List<Integer> values) {
+        return getMap(values).entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> getUniqueValues(List<Integer> values) {
+        return getMap(values).entrySet().stream()
+//                .filter(entry -> entry.getValue() < 2)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
+    }
+
+    private static Map<Integer, Integer> getMap(List<Integer> randomList) {
+        Map<Integer, Integer> result = new HashMap<>();
+        randomList.forEach(key -> result.put(key, result.getOrDefault(key, 0) + 1));
+        return result;
+    }
+}
